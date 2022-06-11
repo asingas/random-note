724309b5f12b87fef06defc64ad9cb8e9f55d2b7
asingas-oracle <arthur.singas@oracle.com>
2022-06-11 11:59:17 -0400

    Base implementation (note/string picker at specified interval)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+const getRandomInt = (arr, max) => {
+  return arr[Math.floor(Math.random() * max)];
+}
+
+const Input = ({setFrequency}) => {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setFrequency(event.target.value * 1000);
+    }
+  }
+  return <input type="number" onKeyDown={handleKeyDown} />
+}
 
 function App() {
+
+  const notes = ['E', 'E#', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B', 'C', 'Cb', 'C#', 'D', 'Db', 'D#','Eb'];
+  const strings = ['High E', 'A', 'D', 'G', 'B', 'E'];
+
+  const [frequency, setFrequency] = useState(5000);
+
+  const [note, setNote] = useState('');
+  const [string, setString] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNote(getRandomInt(notes, notes.length));
+      setString(getRandomInt(strings, strings.length));
+    }, frequency)
+    return () => {
+      clearInterval(interval);
+    }
+  }, [frequency])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Input setFrequency={setFrequency} />
+        <p>{note}</p> 
+        <p>{string}</p>
       </header>
     </div>
   );
