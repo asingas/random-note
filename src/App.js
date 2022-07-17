fed7a61ebfdc48ba4232e05c26d8a22edca1c835
Arthur Singas <singasart@gmail.com>
2022-07-16 21:26:42 -0400

    Fix reset issue with progress bar, still kinda bugged

--- src/App.js
+++ src/App.js
@@ -1,98 +1,101 @@
 import './App.css';
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 /* Helper Functions */
 const getRandomInt = (arr, max) => {
-  return arr[Math.floor(Math.random() * max)];
+    return arr[Math.floor(Math.random() * max)];
 }
 
 const Progress = (props) => {
 
-  const [completed, setCompleted] = useState(0);
-  const refreshRate = 10;
+    const [completed, setCompleted] = useState(0);
+    const refreshRate = 50;
+    const [start, setStart] = useState(Date.now());
 
-  const progressFillStyles = {
-    height: '100%',
-    width: `${completed}%`,
-    backgroundColor: "#00695c",
-    borderRadius: 'inherit',
-    textAlign: 'right'
-  }
-
-  const getPercentComplete = (start) => {
-    const value = Math.floor(((Date.now() - start) / props.frequency) * 100);
-    if (value === 100) {
-      return 0;
-    } else {
-      return value;
+    const progressFillStyles = {
+        height: '100%',
+        width: `${completed}%`,
+        backgroundColor: "#00695c",
+        borderRadius: 'inherit',
+        textAlign: 'right'
     }
-  }
 
-  useEffect(() => {
-    const start = Date.now();
-    const interval = setInterval(() => {
-      setCompleted(getPercentComplete(start));
-    }, refreshRate)
-    return () => {
-      clearInterval(interval);
+    const getPercentComplete = () => {
+        const value = Math.floor(((Date.now() - start) / props.frequency) * 100);
+        console.log(value);
+
+        if (value == 100) {
+            setStart(Date.now());
+            return 0;
+        } else {
+            return value;
+        }
     }
-  }, [props.frequency])
 
-  return (
-    <div className="progress-container">
-      <p>{completed}</p>
-      <div style={progressFillStyles}>
-        <span className='progress-label'>{`${completed}%`}</span>
-      </div>
-    </div>
-  )
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCompleted(getPercentComplete());
+        }, refreshRate)
+        return () => {
+            clearInterval(interval);
+        }
+    }, [props.frequency, start])
+
+    return (
+        <div className="progress-container">
+            <p>{completed}</p>
+            <div style={progressFillStyles}>
+                <span className='progress-label'>{`${completed}%`}</span>
+            </div>
+        </div>
+    )
 }
 
-const Input = ({setFrequency}) => {
+const Input = ({ setFrequency }) => {
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setFrequency(event.target.value * 1000);
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setFrequency(event.target.value * 1000);
+        }
     }
-  }
-  return (
-    <input type="number" onKeyDown={handleKeyDown} />
-  )
+    return (
+        <input type="number" onKeyDown={handleKeyDown} />
+    )
 }
 
 function App() {
-  const notes = [
-    'E', 'E#', 'F', 'F#', 'Gb', 'G', 
-    'G#', 'Ab', 'A', 'A#', 'Bb', 'B', 
-    'C', 'Cb', 'C#', 'D', 'Db', 'D#','Eb'
-  ];
-  const strings = ['Low E', 'A', 'D', 'G', 'B', 'High E'];
+    const notes = [
+        'E', 'E#', 'F', 'F#', 'Gb', 'G',
+        'G#', 'Ab', 'A', 'A#', 'Bb', 'B',
+        'C', 'Cb', 'C#', 'D', 'Db', 'D#', 'Eb'
+    ];
+    const strings = ['Low E', 'A', 'D', 'G', 'B', 'High E'];
 
-  const [frequency, setFrequency] = useState(5000);
-  const [note, setNote] = useState(notes[0]);
-  const [string, setString] = useState(strings[0]);
+    const [frequency, setFrequency] = useState(5000);
+    const [note, setNote] = useState(notes[0]);
+    const [string, setString] = useState(strings[0]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setNote(getRandomInt(notes, notes.length));
-      setString(getRandomInt(strings, strings.length));
-    }, frequency)
-    return () => {
-      clearInterval(interval);
-    }
-  }, [frequency])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNote(getRandomInt(notes, notes.length));
+            setString(getRandomInt(strings, strings.length));
+        }, frequency)
+        return () => {
+            clearInterval(interval);
+        }
+    }, [frequency])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Current frequency: {frequency / 1000} seconds</p> 
-        <Input setFrequency={setFrequency} />
-        <p>{note} on {string} string</p>
-        {/* <Progress frequency={frequency} /> */}
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Current frequency: {frequency / 1000} seconds</p>
+                <Input setFrequency={setFrequency} />
+                <p>{note} on {string} string</p>
+                <Progress frequency={frequency} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
