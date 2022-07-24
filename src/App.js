489196d7e668265fc724aa4e151d2df773655cc7
Arthur Singas <40192855+asingas@users.noreply.github.com>
2022-07-23 20:18:54 -0400

    Fix string/note alignment with frequency, fix bugs (#2)

--- src/App.js
+++ src/App.js
@@ -7,23 +7,20 @@ const getRandomInt = (arr, max) => {
     return arr[Math.floor(Math.random() * max)];
 }
 
-const Progress = (props) => {
-
-    const [completed, setCompleted] = useState(0);
-    const refreshRate = 50;
-    const [start, setStart] = useState(Date.now());
+const Progress = ({ frequency, percentCompleted, setPercentCompleted, start, setStart }) => {
+    const refreshRate = 10;
 
     const progressFillStyles = {
         height: '100%',
-        width: `${completed}%`,
+        width: `${percentCompleted}%`,
         backgroundColor: "#00695c",
         borderRadius: 'inherit',
-        textAlign: 'right'
+        behavior: 'smooth'
+        // textAlign: 'right'
     }
 
     const getPercentComplete = () => {
-        const value = Math.floor(((Date.now() - start) / props.frequency) * 100);
-        console.log(value);
+        const value = Math.floor(((Date.now() - start) / frequency) * 100);
 
         if (value == 100) {
             setStart(Date.now());
@@ -35,28 +32,30 @@ const Progress = (props) => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCompleted(getPercentComplete());
+            setPercentCompleted(getPercentComplete());
         }, refreshRate)
         return () => {
             clearInterval(interval);
         }
-    }, [props.frequency, start])
+    }, [frequency, start])
 
     return (
         <div className="progress-container">
-            <p>{completed}</p>
+            <p>{percentCompleted}</p>
             <div style={progressFillStyles}>
-                <span className='progress-label'>{`${completed}%`}</span>
+                <span className='progress-label'>{`${percentCompleted}%`}</span>
             </div>
         </div>
     )
 }
 
-const Input = ({ setFrequency }) => {
+const Input = ({ setFrequency, setPercentCompleted, setStart }) => {
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             setFrequency(event.target.value * 1000);
+            setPercentCompleted(0);
+            setStart(Date.now());
         }
     }
     return (
@@ -64,15 +63,14 @@ const Input = ({ setFrequency }) => {
     )
 }
 
-function App() {
+const NotesAndStrings = ({ frequency }) => {
     const notes = [
-        'E', 'E#', 'F', 'F#', 'Gb', 'G',
-        'G#', 'Ab', 'A', 'A#', 'Bb', 'B',
-        'C', 'Cb', 'C#', 'D', 'Db', 'D#', 'Eb'
+        'E', 'E#', 'F', 'F#', 'Gb', 'G','G#', 'Ab',
+        'A', 'A#', 'Bb', 'B', 'C', 'Cb', 'C#', 'D', 
+        'Db', 'D#', 'Eb'
     ];
     const strings = ['Low E', 'A', 'D', 'G', 'B', 'High E'];
 
-    const [frequency, setFrequency] = useState(5000);
     const [note, setNote] = useState(notes[0]);
     const [string, setString] = useState(strings[0]);
 
@@ -87,12 +85,43 @@ function App() {
     }, [frequency])
 
     return (
+        <p>{note} on {string} string</p>
+    );
+
+}
+
+const NoteRecognitionPractice = () => {
+    const [percentCompleted, setPercentCompleted] = useState(0);
+    const [frequency, setFrequency] = useState(5000);
+    const [start, setStart] = useState(Date.now());
+
+    return (
+        <div className="note-recognition-practice">
+            <p>Current frequency: <strong>{frequency / 1000}</strong> seconds</p>
+            <Input 
+                setFrequency={setFrequency}
+                setPercentCompleted={setPercentCompleted}
+                setStart={setStart}
+            />
+            <NotesAndStrings 
+                frequency={frequency}
+            />
+            <Progress 
+                frequency={frequency}
+                percentCompleted={percentCompleted}
+                setPercentCompleted={setPercentCompleted}
+                start={start}
+                setStart={setStart}
+            />
+        </div>
+    )
+}
+
+function App() {
+    return (
         <div className="App">
             <header className="App-header">
-                <p>Current frequency: {frequency / 1000} seconds</p>
-                <Input setFrequency={setFrequency} />
-                <p>{note} on {string} string</p>
-                <Progress frequency={frequency} />
+                <NoteRecognitionPractice />
             </header>
         </div>
     );
